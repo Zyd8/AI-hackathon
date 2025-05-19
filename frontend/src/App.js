7e9297175f72fb554383dd28027adbb59f6f344a
Zyldjan Lanticse <115306019+Zyd8@users.noreply.github.com>
2025-05-20 05:39:55 +0800

    [Remove] Dummy auth flag, as it bugs out and remove deprecated nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Dashboard from './Dashboard';
 import BuildingNavigation from './building';
-import Room from './room-deprecated';
-import AdminLogin from './admin-login'; // Import the Admin Login page
+import DevicesPage from './devices';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  // Simulate authentication based on login status
-  const handleLogin = () => {
-    setIsAuthenticated(true);
-  };
-
   return (
     <Router>
       <Routes>
-        {/* Route for Admin Login */}
-        <Route 
-          path="/login" 
-          element={<AdminLogin onLogin={handleLogin} />} 
-        />
-
-        {/* Protected Routes (Only accessible after login)
-        <Route 
-          path="/dashboard" 
-          element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />} 
-        /> */}
-        <Route 
-          path="/building" 
-          element={isAuthenticated ? <BuildingNavigation/> : <Navigate to="/login" />} 
-        />
-        {/* <Route 
-          path="/room" 
-          element={isAuthenticated ? <Room /> : <Navigate to="/login" />} 
-        /> */}
-        
-        {/* Default Route (Redirects to login if the user is not authenticated) */}
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/building" element={<BuildingNavigation/>} />
+        <Route path="/devices/:roomId" element={<DevicesPage />} />
+        <Route path="/" element={<Navigate to="/building" />} />
       </Routes>
     </Router>
   );
