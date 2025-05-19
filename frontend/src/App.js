696af8c94faf7bcaa031331ba5b19f871e71fbb5
Zyldjan Lanticse <115306019+Zyd8@users.noreply.github.com>
2025-05-20 04:57:29 +0800

    [Comment[ Out deprecated navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import BuildingNavigation from './building';
-import Room from './room';
+import Room from './room-deprecated';
 import AdminLogin from './admin-login'; // Import the Admin Login page
 
 function App() {
@@ -22,19 +22,19 @@ function App() {
           element={<AdminLogin onLogin={handleLogin} />} 
         />
 
-        {/* Protected Routes (Only accessible after login) */}
+        {/* Protected Routes (Only accessible after login)
         <Route 
           path="/dashboard" 
           element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />} 
-        />
+        /> */}
         <Route 
           path="/building" 
           element={isAuthenticated ? <BuildingNavigation/> : <Navigate to="/login" />} 
         />
-        <Route 
+        {/* <Route 
           path="/room" 
           element={isAuthenticated ? <Room /> : <Navigate to="/login" />} 
-        />
+        /> */}
         
         {/* Default Route (Redirects to login if the user is not authenticated) */}
         <Route path="/" element={<Navigate to="/login" />} />
